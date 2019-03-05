74a13d2ea62422ba8d3a3f69038417636d0d7033
Crystal Neal <cdneal091@gmail.com>
2019-03-05 18:05:54 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,16 @@
+require 'pry'
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    animals = []
+    #animals << self.key(*arguments)
+    arguments.each do |argument|
+    self.each do | creature , place |
+      if place == argument
+        animals << creature
+      #binding.pry
+      end
+    end
+    end
+    animals
   end
 end
