fdd39161900f4de4f18cf83e5ebf5c89b6249974
github username <github email address>
2019-03-09 14:47:27 -0700

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    result = []
+
+    #Use self to call each on whatever hash is currently inside the method that
+    #was passed to it as an argument
+    self.each do |key, value|
+      if arguments.include?(value)
+        result << key
+      end
+    end
+    return result
   end
 end
