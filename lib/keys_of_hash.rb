7f00ce63120f71521f3eb712038ead8449477dae
yeah-right <claar.zack@gmail.com>
2019-03-30 12:17:42 -0600

    tests passing

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,26 @@
+
+#
+# class Hash
+#   def keys_of(arguments)
+#     # code goes here
+#     if arguments == "Panama"
+#       return ['red-footed tortoise']
+#     elsif arguments == 'Madagascar'
+#       return ["aye-aye", 'tomato frog']
+#     elsif arguments == "Australia"
+#       return ["sugar glider", "kangaroo", "koala"]
+#     elsif arguments.is_a?(Hash)
+#       return arguments.keys
+#     else
+#     return []
+#     end
+#   end
+# end
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*args)
+    map do |key, value|
+      args.include?(value) ? key : nil
+    end.compact
   end
 end
